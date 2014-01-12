d3ecbd725c658925ae2db3aa7609e13ffea2aede
Kimberley Ong <kimmey@Kimberleys-MacBook-Air-2.local>
2014-01-12 18:27:26 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ appHelper.runApp(function(app, db) {
 	console.log("My app is running!");
 
 	app.get("/hello", function (req, resp) {
-		resp.write("HELLO WORLD!");
+		resp.write("HELLO WORLD! My name is Kim.");
 		resp.end();
+	});
+
+	app.get("/html", function (req, resp) {
+		resp.write("<html><body><h1>Woo! Node is cool!</h1></body></html><button>Click me</button>");
+		resp.end();
+	});
+
+	app.get("/template/:name", function (req, resp) {
+		resp.render("template", {name: req.params.name});
+	});
+
+	app.get("/", function (req, resp) {
+		resp.render("index");
 	});
 })
